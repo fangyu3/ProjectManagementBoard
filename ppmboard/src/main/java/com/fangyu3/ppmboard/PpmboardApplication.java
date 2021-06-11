00f8dffa60f13613f3f7d8e7fd0dc7e8a99413c0
fangyu3 <tofang23@gmail.com>
2021-06-11 11:23:32 -0400

    Initial commit

---
+++ ppmboard/src/main/java/com/fangyu3/ppmboard/PpmboardApplication.java
@@ -0,0 +1,13 @@
+package com.fangyu3.ppmboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmboardApplication.class, args);
+	}
+
+}
---
+++ ppmboard/src/test/java/com/fangyu3/ppmboard/PpmboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fangyu3.ppmboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
